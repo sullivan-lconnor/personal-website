96449adeb6588088715b0cf309d1b7e69f2a46ec
sullivan-lconnor <sullivan.lconnor@gmail.com>
2024-05-27 15:20:41 -0400

    update server.js to allow traffic

--- personal-website-express/server.js
+++ personal-website-express/server.js
@@ -11,6 +11,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
